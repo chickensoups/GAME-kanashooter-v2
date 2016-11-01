2f673b0bf72f89fee41c3f8214f69ac8cee4527f
DVBao-PC\DVBao <sieusaoda@gmail.com>
2016-11-01 22:51:04 +0700

    _Scene: -them Levels scene
    tutorial scene:
    -xoa ButtonPanel (1)
    -tao HomeButtonsSystem thay the cho ButtonPanel (1)
    -them HomeButtonsSystem
    
    prefabs:
    -them HomeButtonsSystem
    
    Script:
    -them HomeResumeButtonController
    -sua MenuButtonController

---
+++ Assets/Script/HomeResumeButtonController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HomeResumeButtonController : MonoBehaviour {
+    //home button is the small button at high right conner
+    //Resume button is the whole screen + make the screen darker
+    public Button HomeButton, ResumeButton;
+    public GameObject MenuPanel;
+    void Start()
+    {
+        ResumeButton.gameObject.SetActive(false);
+        HomeButton.onClick.AddListener(openMenuPanel);
+    }
+    void Update()
+    {   
+        //to make sure that newly activated buttons work properly
+        if(HomeButton.gameObject.activeInHierarchy)
+        {
+            HomeButton.onClick.AddListener(openMenuPanel);
+        }
+        if(ResumeButton.gameObject.activeInHierarchy)
+        {
+            ResumeButton.onClick.AddListener(closeMenuPanel);
+        }
+    }
+    void openMenuPanel()
+    {
+        MenuPanel.SetActive(true);
+        //deactivate Home button, activate Resume button
+        HomeButton.gameObject.SetActive(false);
+        ResumeButton.gameObject.SetActive(true);
+        //Pause scene
+        Time.timeScale = 0;
+    }
+    void closeMenuPanel()
+    {
+        MenuPanel.SetActive(false);
+        //deactivate Resume button, activate Home button
+        HomeButton.gameObject.SetActive(true);
+        ResumeButton.gameObject.SetActive(false);
+        //Stop pausing scene
+        Time.timeScale = 1;
+    }
+}
