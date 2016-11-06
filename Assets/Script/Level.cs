6102143a22df78c6d6f9e7f2579ef5038ac1dcf3
vietcoscc <vietcoscc@gmail.com>
2016-11-06 16:22:22 +0700

    Hoàn thành Select Level Scene

---
+++ Assets/Script/Level.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    class Level
+    {
+        public bool isUnlocked;
+        public int index;
+    }
---
+++ Assets/Script/LevelSelectController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelSelectController : MonoBehaviour
+{
+    Level[] levels = new Level[50];
+    public GameObject levelUnlockedPrefab;
+    public GameObject levelLockedPrefab;
+    private GameObject highestUnlockedLevel;
+    // Use this for initialization
+    void Start()
+    {
+        int currentUnlockedLevel = 10;
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject levelPrefab;
+            levels[i] = new Level();
+            levels[i].index = i;
+            if (i < currentUnlockedLevel)
+            {
+                levels[i].isUnlocked = true;
+            }
+
+
+            if (levels[i].isUnlocked)
+            {
+                levelPrefab = Instantiate(levelUnlockedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                levelPrefab.GetComponentInChildren<Text>().text = i + 1+"";
+            }
+            else
+            {
+                levelPrefab = Instantiate(levelLockedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                
+            }
+            if (i == currentUnlockedLevel - 1)
+            {
+                highestUnlockedLevel = levelPrefab;
+            }
+            levelPrefab.transform.parent = transform;
+            Button btn = levelPrefab.GetComponent<Button>();
+            btn.onClick.AddListener(OnButtonClick);
+        }
+
+      //  Vector3 positionScroll = highestUnlockedLevel.transform.position;
+     //   GetComponent<RectTransform>().position = new Vector3(transform.position.x,270, transform.position.z) ;
+ 
+
+    }
+    void OnButtonClick()
+    {
+        Debug.Log("It works");
+        //To do ...
+    }
+   
+}
+
+
