99e4ba75b3f2f981f15668b843e96d918f3e6ead
VELACIELA\SonBN <sonbn.225@gmail.com>
2016-11-06 16:34:45 +0700

    Complete raw Learn Scene

--- Assets/Script/KanaScript.cs
+++ Assets/Script/KanaScript.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using UnityEditorInternal;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class KanaScript : MonoBehaviour
 {
+    public Button HiraganaTab, KatakanaTab;
+    public GameObject prefabs;
+    public static string[] ROMA_CHARS = { "a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", ".", "yu", ".", "yo", "ra", "ri", "ru", "re", "ro", "wa", ".", "n", ".", "wo" };
+    public static string[] HIRA_CHARS = { "あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", ".", "ゆ", ".", "よ", "ら", "り", "る", "れ", "ろ", "わ", ".", "ん", ".", "を"};
+    public static string[] KATA_CHARS = { "ア", "イ", "ウ", "エ", "オ", "カ", "キ", "ク", "ケ", "コ", "サ", "シ", "ス", "セ", "ソ", "タ", "チ", "ツ", "テ", "ト", "ナ", "ニ", "ヌ", "ネ", "ノ", "ハ", "ヒ", "フ", "ヘ", "ホ", "マ", "ミ", "ム", "メ", "モ", "ヤ", ".", "ユ", ".", "ヨ", "ラ", "リ", "ル", "レ", "ロ", "ワ", ".", "ン", ".", "ヲ" };
+
     void Start()
     {
+        initTable();
+        HiraganaTab.GetComponent<Button>();
+        KatakanaTab.GetComponent<Button>();
+        HiraganaTab.onClick.AddListener(clickHiraganaButton);
+        KatakanaTab.onClick.AddListener(clickKatakanaButton);
         GameObject[] characters = GameObject.FindGameObjectsWithTag("Character");
         for (int i = 0; i < characters.Length; i++)
         {
@@ -19,10 +31,64 @@ public class KanaScript : MonoBehaviour
     void ClickCharacter()
     {
 
-        String character = EventSystem.current.currentSelectedGameObject.name.ToLower();
+        String character = EventSystem.current.currentSelectedGameObject.name;
         
         AudioClip audio = Resources.Load<AudioClip>("sound/pronunciation/" + character);
-        AudioSource.PlayClipAtPoint(audio, new Vector3(0, 0, 0) );
+        AudioSource.PlayClipAtPoint(audio, new Vector3(0, 0, 0), 0.5f);
     }
 
+    void initTable()
+    {
+        for (int i = 0; i < ROMA_CHARS.Length; i++)
+        {
+            if (ROMA_CHARS[i] != ".")
+            {
+                GameObject kana = Instantiate(prefabs, Vector3.zero, Quaternion.identity) as GameObject;
+                kana.transform.parent = transform;
+                kana.transform.localScale = new Vector3(1, 1);
+                kana.name = ROMA_CHARS[i];
+                kana.GetComponentsInChildren<Text>()[0].text = HIRA_CHARS[i];
+                kana.GetComponentsInChildren<Text>()[1].text = ROMA_CHARS[i];
+            }
+            else
+            {
+                GameObject kana = Instantiate(prefabs, Vector3.zero, Quaternion.identity) as GameObject;
+                kana.transform.parent = transform;
+                kana.transform.localScale = new Vector3(1, 1);
+                kana.GetComponentInChildren<Image>().enabled = false;
+                kana.GetComponentsInChildren<Text>()[0].text = "";
+                kana.GetComponentsInChildren<Text>()[1].text = "";
+            }
+        }
+    }
+
+    void clickHiraganaButton()
+    {
+        GameObject[] characters = GameObject.FindGameObjectsWithTag("Character");
+        if (characters.Length != 0)
+        {
+            for (int i = 0; i < ROMA_CHARS.Length; i++)
+            {
+                if (ROMA_CHARS[i] != ".")
+                {
+                    characters[i].GetComponentsInChildren<Text>()[0].text = HIRA_CHARS[i];
+                }
+            }
+        }
+    }
+
+    void clickKatakanaButton()
+    {
+        GameObject[] characters = GameObject.FindGameObjectsWithTag("Character");
+        if (characters.Length != 0)
+        {
+            for (int i = 0; i < ROMA_CHARS.Length; i++)
+            {
+                if (ROMA_CHARS[i] != ".")
+                {
+                    characters[i].GetComponentsInChildren<Text>()[0].text = KATA_CHARS[i];
+                }
+            }
+        }
+    }
 }
