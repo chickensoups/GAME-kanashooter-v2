d93f395b8efddb6ee9c97cbecece7e78f41b4fe8
DVBao-PC\DVBao <sieusaoda@gmail.com>
2016-11-17 17:45:56 +0700

    -Scripts:   -Add ReturnMenuButtonController -Scenes:   -Tutorial and Levels get returnMenu buttons -Prefabs:   -Add ReturnMenu in New

---
+++ Assets/Script/ReturnMenuButtonController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnMenuButtonController : MonoBehaviour {
+
+    public Button returnMeunuButton;
+
+    void Start()
+    {
+        returnMeunuButton.onClick.AddListener(goToMenu);
+    }
+    public void goToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
