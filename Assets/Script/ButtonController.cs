508f1f0e6f458df316500637dd706abf18f03316
VELACIELA\SonBN <sonbn.225@gmail.com>
2016-11-17 19:34:34 +0700

    Fixed main menu button panel

--- Assets/Script/ButtonController.cs
+++ Assets/Script/ButtonController.cs
@@ -1,33 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonController: MonoBehaviour
 {
-    public Button PlayButton, TutorialButton, LearnButton, MainMenuButton;
+    public Button PlayButton, SelectLevelButton, TutorialButton, LearnButton;
 
     void Start()
     {
         Button btnPlay = PlayButton.GetComponent<Button>();
+        Button btnSelectLevel = SelectLevelButton.GetComponent<Button>();
         Button btnTutorial = TutorialButton.GetComponent<Button>();
         Button btnLearn = LearnButton.GetComponent<Button>();
-        Button btnMainMenu = MainMenuButton.GetComponent<Button>();
+
         btnPlay.onClick.AddListener(Play);
+        btnSelectLevel.onClick.AddListener(SelectLevel);
         btnTutorial.onClick.AddListener(Tutorial);
         btnLearn.onClick.AddListener(Learn);
-        btnMainMenu.onClick.AddListener(BackToMainMenu);
 
-    }
-
-    public void BackToMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
     }
 
     public void Play()
     {
         SceneManager.LoadScene("Play");
+    }
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene("Levels");
     }
 
     public void Tutorial()
