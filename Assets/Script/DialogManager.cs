b274f05d5895b5ad5880537500fd0018269af2ec
vietcoscc <vietcoscc@gmail.com>
2016-11-20 11:36:27 +0700

    Improve LevelSelect Scene

--- Assets/Script/DialogManager.cs
+++ Assets/Script/DialogManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
-
+using UnityEngine.EventSystems;
 public class DialogManager : MonoBehaviour {
     private Button btnClose;
     private Text welcomeMessage;
@@ -11,42 +11,83 @@ public class DialogManager : MonoBehaviour {
     public GameObject questionPrefab;
     public Transform questionsTransform;
     public Transform answersTransform;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private static DialogManager _instance;
+
+    public static DialogManager instance
+    {
+        get { return _instance; }
+    }
+
+    public GameObject currentTarget;
+
+    public void ShowData()
+    {
+
+        foreach(Transform child in answersTransform)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in questionsTransform)
+        {
+            Destroy(child.gameObject);
+        }
+
+       
         level = LevelSelectController.currentViewInfoLevel;
         btnClose = GetComponentInChildren<Button>();
-        btnClose.onClick.AddListener(OnCloseButton);
+        btnClose.onClick.AddListener(OnCloseButtonClick);
         GetComponentsInChildren<Text>()[1].text = level.GetWelcomeMessage();
         Debug.Log(GetComponentInChildren<Text>().text);
-       
-        
+
+
 
         for (int i = 0; i < level.GetAnswers().Count; i++)
         {
             GameObject answerTemp;
             answerTemp = Instantiate(answerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
             answerTemp.transform.SetParent(answersTransform);
-            answerTemp.transform.localScale = new Vector3(1,1);
+            answerTemp.transform.localScale = new Vector3(1, 1);
             answerTemp.GetComponentInChildren<Text>().text = level.GetAnswers()[i];
+            Debug.Log(level.GetAnswers()[i]);
+
+
+            AudioSource audioSource = answerTemp.AddComponent<AudioSource>();
+            audioSource.clip = Resources.Load("sound/pronunciation/"+level.GetAnswers()[i]) as AudioClip;
+            Button btn = answerTemp.GetComponent<Button>();
+            btn.onClick.AddListener(OnAnswerButtonClick);
         }
 
         for (int i = 0; i < level.GetAnswers().Count; i++)
         {
             GameObject questionTemp;
-            questionTemp =  Instantiate(questionPrefab,Vector3.zero,Quaternion.identity) as GameObject;
+            questionTemp = Instantiate(questionPrefab, Vector3.zero, Quaternion.identity) as GameObject;
             questionTemp.transform.SetParent(questionsTransform);
             questionTemp.transform.localScale = new Vector3(1, 1);
-            questionTemp.GetComponentInChildren<Text>().text = level.GetAnswers()[i];
+            questionTemp.GetComponentInChildren<Text>().text = level.GetQuestions()[i];
+
+            AudioSource audioSource = questionTemp.AddComponent<AudioSource>();
+            audioSource.clip = Resources.Load("sound/pronunciation/" + level.GetAnswers()[i]) as AudioClip;
+            Button btn = questionTemp.GetComponent<Button>();
+            btn.onClick.AddListener(OnAnswerButtonClick);
         }
-	}
+    }
+	
 	
 	// Update is called once per frame
 	void Update () {
 	    
 	}
 
-    void OnCloseButton()
+    void OnCloseButtonClick()
     {
+        level = LevelSelectController.currentViewInfoLevel;
         gameObject.SetActive(false);
+    }
+    void OnAnswerButtonClick()
+    {
+        GameObject btn = EventSystem.current.currentSelectedGameObject;
+        AudioSource clip = btn.GetComponent<AudioSource>();
+        clip.Play();
     }
 }
--- Assets/Script/LevelSelectController.cs
+++ Assets/Script/LevelSelectController.cs
@@ -14,48 +14,56 @@ public class LevelSelectController : MonoBehaviour
     private GameObject highestUnlockedLevel;
     public GameObject infoPanel;
     public static Level currentViewInfoLevel;
+    private int currentUnlockedLevel = 5;
+    private GameObject levelPrefab;
     // Use this for initialization
     void Start()
     {
         LevelUtils.initLevel();
-        int currentUnlockedLevel = 10;
-        for (int i = 0; i < 20; i++)
+
+        for (int i = 0; i < LevelUtils.GetLevels().Count; i++)
         {
-            GameObject levelPrefab;
-            levels[i] = new Level();
-			levels[i].SetIndex(i) ;
-            if (i < currentUnlockedLevel)
-            {
-				levels[i].SetIsUnlocked(true);
-            }
+            CreateSelectLevel(i);
+        }
+    }
+    void CreateSelectLevel(int i)
+    {
+        levels[i] = new Level();
+        levels[i].SetIndex(i);
+        if (i < currentUnlockedLevel)
+        {
+            levels[i].SetIsUnlocked(true);
+        }
 
+        if (levels[i].IsUnlocked())
+        {
+            levelPrefab = Instantiate(levelUnlockedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+            levelPrefab.GetComponentInChildren<Text>().text = i + 1 + "";
 
-			if (levels[i].IsUnlocked())
-            {
-                levelPrefab = Instantiate(levelUnlockedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
-                levelPrefab.GetComponentInChildren<Text>().text = i + 1+"";
-                Button btnInfo = levelPrefab.GetComponentsInChildren<Button>()[1];
-                btnInfo.onClick.AddListener(OnButtonInfoClick);
-            }
-            else
-            {
-                levelPrefab = Instantiate(levelLockedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
-            }
-            if (i == currentUnlockedLevel - 1)
-            {
-                highestUnlockedLevel = levelPrefab;
-            }
-            levelPrefab.transform.parent = transform;
-            levelPrefab.transform.localScale = new Vector3(1, 1);
             Button btn = levelPrefab.GetComponent<Button>();
             btn.onClick.AddListener(OnButtonClick);
 
-            
+            Button btnInfo = levelPrefab.GetComponentsInChildren<Button>()[1];
+            btnInfo.onClick.AddListener(OnButtonInfoClick);
         }
-
+        else
+        {
+            levelPrefab = Instantiate(levelLockedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        }
+        if (i == currentUnlockedLevel - 1)
+        {
+            highestUnlockedLevel = levelPrefab;
+        }
+        levelPrefab.transform.parent = transform;
+        levelPrefab.transform.localScale = new Vector3(1, 1);
+        if (i == LevelUtils.GetLevels().Count - 1 || i == LevelUtils.GetLevels().Count - 2)
+        {
+            levelPrefab.GetComponent<Image>().color = Color.red;
+        }
     }
     void OnButtonClick()
     {
+        
         int levelNumber = Int32.Parse(EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text);
         LevelUtils.currentLevel = LevelUtils.GetLevel(levelNumber);
         SceneManager.LoadScene("Play");
@@ -66,10 +74,9 @@ public class LevelSelectController : MonoBehaviour
         infoPanel.SetActive(true);
         GameObject goParent = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
         int levelNumber = Int32.Parse(goParent.GetComponentInChildren<Text>().text);
-      
+        Debug.Log(levelNumber);
         currentViewInfoLevel= LevelUtils.GetLevel(levelNumber);
-        
-
+        infoPanel.GetComponent<DialogManager>().ShowData();
     }
 }
 
